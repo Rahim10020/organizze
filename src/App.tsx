bad58480296edb5b2aa3bece0ae43de72aa89eb3
Rahim10020 <rahialighi@gmail.com>
2026-03-21 11:55:42 +0000

    codging Section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { DocLink } from "./shared/components/DocLink";
 import { Logo } from "./shared/components/Logo";
+import { Section } from "./shared/components/Section";
 import { UserMeta } from "./shared/components/UserMeta";
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
       <Logo />
       <UserMeta />
       <DocLink href="#">Moi</DocLink>
+      <Section title="Notre avenir">
+        <p>
+          Je ne sais toujours pas comment faire tourner le ballon de basket sur
+          mon doigt
+        </p>
+      </Section>
     </>
   );
 }
--- src/shared/components/Section.tsx
+++ src/shared/components/Section.tsx
@@ -1,1 +1,18 @@
+import type { ReactNode } from "react";
+
 // h2 + content
+export type SectionProps = {
+  title: string;
+  children: ReactNode;
+  id?: string;
+};
+
+export function Section(props: SectionProps) {
+  const { title, children, id } = props;
+  return (
+    <section id={id} className="">
+      <h2>{title}</h2>
+      <div>{children}</div>
+    </section>
+  );
+}
