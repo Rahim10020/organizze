ee5ad9279737d7637acdd86a8b5a6e094779bf5b
Rahim10020 <rahialighi@gmail.com>
2026-03-21 15:12:48 +0000

    redesign user meta

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -8,9 +8,9 @@ export function Logo(props: LogoProps) {
   const iconSize = size === "sm" ? "h-5 w-5" : "h-8 w-8";
 
   return (
-    <div className="flex items-center gap-p1">
+    <div className="flex items-center gap-p4">
       <img src="/images/logo.png" alt="logo image" className={iconSize} />
-      <span className="text-black text-dp24">{name}</span>
+      <span className="text-black-80 text-b20">{name}</span>
     </div>
   );
 }
--- src/shared/components/UserMeta.tsx
+++ src/shared/components/UserMeta.tsx
@@ -11,7 +11,7 @@ export type UserMetaProps = {
 
 export function UserMeta(props: UserMetaProps) {
   const {
-    username = "Rahim",
+    username = "takopi/Rahim",
     version = "3.2.0",
     stars = 2343,
     forks = 167,
@@ -24,27 +24,27 @@ export function UserMeta(props: UserMetaProps) {
         <img
           src="/images/avatar.png"
           alt="avatar image"
-          className="w-10 h-10 rounded-full"
+          className="w-8 h-8 rounded-full"
         />
       </div>
       {/* right */}
       <div className="flex flex-col gap-p0-5">
         {/* username */}
         <div>
-          <span>{username}</span>
+          <span className="text-m14 text-black-80">{username}</span>
         </div>
         {/* stats */}
-        <div className="flex items-center gap-p1-5">
+        <div className="flex items-center gap-p1-5 text-l14 text-black-60">
           <div className="flex items-center gap-0.5">
-            <TagIcon className="w-5 h-5" />
+            <TagIcon className="w-3 h-3" />
             <span>{version}</span>
           </div>
           <div className="flex items-center gap-0.5">
-            <StarIcon className="w-5 h-5" />
+            <StarIcon className="w-3 h-3" />
             <span>{stars}</span>
           </div>
           <div className="flex items-center gap-0.5">
-            <BookmarkIcon className="w-5 h-5" />
+            <BookmarkIcon className="w-3 h-3" />
             <span>{forks}</span>
           </div>
         </div>
