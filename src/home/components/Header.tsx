8a06eb89521fe577481bcc2e7065b5d95bc95bb9
Rahim10020 <rahialighi@gmail.com>
2026-03-25 14:46:15 +0000

    fixes

--- src/home/components/Header.tsx
+++ src/home/components/Header.tsx
@@ -14,7 +14,7 @@ const navItems = [
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 border-b border-black-5 bg-white px-p4 lg:px-p8 py-p6">
+    <header className="sticky top-0 z-50 border-b border-black-5 bg-white px-p4 lg:px-p8 pb-p6 pt-p4">
       <div className="mx-auto max-w-7xl w-full">
         {/* first line */}
         <div className="flex items-center justify-between">
@@ -29,7 +29,7 @@ export function Header() {
         </div>
         {/* second line */}
         <nav className="pt-p6">
-          <ul className="flex items-center gap-p6 ">
+          <ul className="flex items-center gap-p8 ">
             {navItems.map((item) => (
               <li key={item.label}>
                 <NavItem
